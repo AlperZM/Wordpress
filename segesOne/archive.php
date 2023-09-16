ed023b592a75295bbac24be5a90873fc0f454517
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-09-16 10:09:48 +0300

    Update archive.php

--- segesOne/archive.php
+++ segesOne/archive.php
@@ -1,4 +1,11 @@
 <?php
+<header class="page-header">
+    <?php
+        the_archive_title( '<h1 class="page-title">', '</h1>' );
+        the_archive_description( '<div class="taxonomy-description">', '</div>' );
+    ?>
+</header>
+
 if ( have_posts() ) :
     while ( have_posts() ) : the_post();
         the_title( '<h2>', '</h2>' );
