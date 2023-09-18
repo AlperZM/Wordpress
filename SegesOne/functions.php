5136e9a032172f97393c48ae94a1a96dd192c658
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-09-18 13:55:13 +0300

    Update functions.php

--- SegesOne/functions.php
+++ SegesOne/functions.php
@@ -1,8 +1,22 @@
 <?php
-if ( ! function_exists( 'SegesOnesetup' ) ) :
-function SegesOnesetup() {
+if ( ! function_exists( 'Seges_One_setup' ) ) :
+function Seges_One_setup() {
 	add_theme_support( 'wp-block-styles' );
 	add_editor_style( 'editor-style.css' );
 }
 endif; 
-add_action( 'after_setup_theme', 'SegesOnesetup' );
+add_action( 'after_setup_theme', 'Seges_One_setup' );
+
+
+/**
+ * Enqueue the style.css file.
+ */
+function Seges_One_styles() {
+	wp_enqueue_style(
+		'Seges-One-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'Seges_One_styles' );
