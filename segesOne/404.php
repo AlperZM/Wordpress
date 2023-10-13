e49973008433826629797e2895701d33468ce0b7
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-10-13 12:54:33 +0300

    Update 404.php

--- segesOne/404.php
+++ segesOne/404.php
@@ -1,19 +1,21 @@
 <?php
 /* The template for displaying 404 pages (not found) */
-
 get_header();
 ?>
 
-	<header class="page-header alignwide">
+<main id="404-main">
+	<section class="error-404">
+		<header class="page-header alignwide">
 		<h1 class="page-title"><?php esc_html_e( 'Sorry about nothing here', 'segesone' ); ?></h1>
-	</header><!-- .page-header -->
-
-	<div class="error-404 not-found default-max-width">
+	</header>
 		<div class="page-content">
 			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'segesone' ); ?></p>
 			<?php get_search_form(); ?>
-		</div><!-- .page-content -->
-	</div><!-- .error-404 -->
+		</div>
+	</section>
+</main>
+	
+<?php get_sidebar(); ?>
 
 <?php
 get_footer();
