4cab982160e4dbccfb89e07328d15fd1e3c99bdb
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-10-13 13:00:42 +0300

    Update archive.php

--- segesOne/archive.php
+++ segesOne/archive.php
@@ -1,18 +1,30 @@
 <?php
-<header class="page-header">
-    <?php
-        the_archive_title( '<h1 class="page-title">', '</h1>' );
-        the_archive_description( '<div class="taxonomy-description">', '</div>' );
-    ?>
-</header>
+/* The template for displaying archive pages */
 
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-        the_title( '<h2>', '</h2>' );
-        the_post_thumbnail();
-        the_excerpt();
-    endwhile;
-else:
-    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-endif;
+get_header();
+
+$description = get_the_archive_description();
 ?>
+
+<?php if ( have_posts() ) : ?>
+
+	<header class="page-header alignwide">
+		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+		<?php if ( $description ) : ?>
+			<article class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></article>
+		<?php endif; ?>
+	</header><!-- .page-header -->
+
+	<?php while ( have_posts() ) : ?>
+		<?php the_post(); ?>
+		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+	<?php endwhile; ?>
+
+	<?php seges_one_the_posts_navigation(); ?>
+
+<?php else : ?>
+	<?php get_template_part( 'template-parts/content/content-none' ); ?>
+<?php endif; ?>
+<?php get_sidebar(); ?>
+<?php
+get_footer();
