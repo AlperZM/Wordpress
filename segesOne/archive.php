c34fa2a417b88f7fdc72586ce7ad833ade94e681
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-10-21 11:30:54 +0300

    Update archive.php

--- segesOne/archive.php
+++ segesOne/archive.php
@@ -1,30 +1,51 @@
 <?php
-/* The template for displaying archive pages */
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Seges_One
+ */
 
 get_header();
-
-$description = get_the_archive_description();
 ?>
 
-<?php if ( have_posts() ) : ?>
+	<main id="primary" class="site-main">
 
-	<header class="page-header alignwide">
-		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-		<?php if ( $description ) : ?>
-			<article class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></article>
-		<?php endif; ?>
-	</header><!-- .page-header -->
+		<?php if ( have_posts() ) : ?>
 
-	<?php while ( have_posts() ) : ?>
-		<?php the_post(); ?>
-		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
-	<?php endwhile; ?>
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
-	<?php seges_one_the_posts_navigation(); ?>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
 
-<?php else : ?>
-	<?php get_template_part( 'template-parts/content/content-none' ); ?>
-<?php endif; ?>
-<?php get_sidebar(); ?>
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
 <?php
+get_sidebar();
 get_footer();
