ebc97fe0a96d3ca4de67787370ca61651c425c68
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-10-21 11:37:50 +0300

    Update sidebar.php

--- segesOne/sidebar.php
+++ segesOne/sidebar.php
@@ -1,3 +1,17 @@
-<div id="sidebar-primary" class="sidebar">
-	<?php dynamic_sidebar( 'primary' ); ?>
-</div>
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Seges_One
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
