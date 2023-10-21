c0497c0033eb9385bad9969e5e7f57c179d84675
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-10-21 11:31:24 +0300

    Update footer.php

--- segesOne/footer.php
+++ segesOne/footer.php
@@ -1,70 +1,48 @@
 <?php
-/* The template for displaying the footer */
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Seges_One
+ */
+
 ?>
-			</main>
-		</div>
-	</div>
 
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
-
-	<footer id="footer" class="site-footer">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'segesone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul>
-			</nav>
-		<?php endif; ?>
+	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<div class="site-name">
-				<?php if ( has_custom_logo() ) : ?>
-					<div class="site-logo"><?php the_custom_logo(); ?></div>
-				<?php else : ?>
-					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-						<?php if ( is_front_page() && ! is_paged() ) : ?>
-							<?php bloginfo( 'name' ); ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						<?php endif; ?>
-					<?php endif; ?>
-				<?php endif; ?>
-			</div>
-
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '<div class="privacy-policy">', '</div>' );
-			}
-			?>
-
-			<div class="powered-by">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'seges-one' ) ); ?>">
 				<?php
-				printf(
-					/* translators: %s: WordPress. */
-					esc_html__( 'Proudly powered by %s.', 'segesone' ),
-					'<a href="' . esc_url( __( 'https://seges.web.tr/', 'segesone' ) ) . '">Seges</a>'
-				);
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'seges-one' ), 'WordPress' );
 				?>
-			</div>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'seges-one' ), 'seges-one', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
 
-		</div>
-	</footer>
-
-</div>
 
 <?php wp_footer(); ?>
 
 </body>
 </html>
+
+<!--  <!-- Footer 
+  <footer id="footer" class="flex-center column">
+    <div class="footer-content flex-center row"><p>&copy; 2023 Tüm hakları saklıdır.</p>
+    <a href="#" class="footer-link">Gizlilik Politikası</a>
+    <a href="#" class="footer-link">Şartlar</a>
+    <a href="#" class="footer-link">Hizmet Şartları</a></div>
+    <div class="footer-content flex-center row">
+      <p>Copyright &copy; 2023 <a href="#">Seges</a>. All rights reserved.</p>
+    </div>
+  </footer>
+  <!-- End Footer 
+  <script src="script.js"></script>
+</body>
