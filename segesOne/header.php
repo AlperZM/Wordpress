dc089a69bfb00527ad32a8641378a5e24bf1505c
AlperZM <35838758+AlperZM@users.noreply.github.com>
2025-04-07 16:54:46 +0300

    Update header.php

--- segesOne/header.php
+++ segesOne/header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Seges_One
+ * @package seges
  */
 
 ?>
@@ -20,42 +20,42 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php //body_class(); ?>>
-<?php //wp_body_open(); ?>
-	
-	<header id="main-header" class="flex-center row">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'seges-one' ); ?></a>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'seges' ); ?></a>
+
+	<header id="header">
 		<div class="site-branding">
-			
-			<?php the_custom_logo(); ?>
-			<?php if ( is_front_page() && is_home() ) :?>
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				
-			<?php endif;
-			$seges_one_description = get_bloginfo( 'description', 'display' );
-			if ( $seges_one_description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $seges_one_description;  ?></p>
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$seges_description = get_bloginfo( 'description', 'display' );
+			if ( $seges_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $seges_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-		
 
-
-	<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'seges-one' ); ?></button>
+		<nav id="site-navigation" class="main-navigation">
+		<?php //	<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'seges' ); </button> ?>
 			<?php
 			wp_nav_menu(
 				array(
-					    'theme_location' => 'primary',
-    					'menu_class' => 'main-navigation flex-center',
-						'menu_id'=> 'main-nav',
-    					'container' => 'nav',
-    					'container_class' => 'flex-center',
- //    					'walker' => new SegesOne_Menu_Walker(),
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'main-navigation-bar',
+					'walker'         => new Custom_Nav_Walker(),
+					
 				)
 			);
 			?>
-
-	
+		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-	
